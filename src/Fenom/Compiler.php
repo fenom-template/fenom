89d1cae918d363143aa0eed80ae0b62cff9f6799
Dmitry Krokhin <nekufa@gmail.com>
2014-06-30 13:36:11 +0400

    Fixed array convertion and foreach tag separator

--- src/Fenom/Compiler.php
+++ src/Fenom/Compiler.php
@@ -144,7 +144,7 @@ class Compiler
                 $check = '!empty('.$from.')';
             } else {
                 $scope["var"] = $scope->tpl->tmpVar();
-                $prepend = $scope["var"].' = (array)('.$from.')';
+                $prepend = $scope["var"].' = (array)('.$from.');';
                 $from = $check = $scope["var"];
             }
         } elseif ($tokens->is('[')) {
