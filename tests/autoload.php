44ac3b16e5a129015d289fee9c6934e93b1cac05
bzick <a.cobest@gmail.com>
2017-04-22 16:39:33 +0300

    Phpunit things

--- tests/autoload.php
+++ tests/autoload.php
@@ -2,8 +2,6 @@
 
 require(__DIR__ . "/../src/Fenom.php");
 Fenom::registerAutoload();
-require_once __DIR__ . "/../vendor/autoload.php";
-
 
 if(!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
     class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
