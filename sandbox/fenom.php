abf3bd5414fb2990be0f1322774f46b59987e97c
bzick <a.cobest@gmail.com>
2017-04-22 15:59:20 +0300

    Fix #260

--- sandbox/fenom.php
+++ sandbox/fenom.php
@@ -6,11 +6,13 @@ require_once __DIR__.'/../tests/tools.php';
 \Fenom::registerAutoload();
 
 $fenom = Fenom::factory(__DIR__.'/templates', __DIR__.'/compiled');
-$fenom->setOptions(Fenom::AUTO_RELOAD | Fenom::FORCE_VERIFY | Fenom::FORCE_INCLUDE);
+//$fenom->setOptions(Fenom::AUTO_RELOAD | Fenom::FORCE_VERIFY | Fenom::FORCE_INCLUDE);
 //var_dump($fenom->compile("nested.tpl", [])->getTemplateCode());
 //exit;
-var_dump($fenom->compile('bug241/recursive.tpl', false)->getBody());
+//var_dump($fenom->compile('bug241/recursive.tpl', false)->getBody());
 //var_dump($fenom->compile('bug249/bread.tpl', false)->getBody());
 //var_dump($fenom->compile("bug158/main.tpl", [])->getTemplateCode());
 //var_dump($fenom->display("bug158/main.tpl", []));
 // $fenom->getTemplate("problem.tpl");
+
+var_dump($fenom->compileCode('{$.php.Fenom::factory()->addDay()}')->getBody());
--- tests/cases/Fenom/RenderTest.php
+++ tests/cases/Fenom/RenderTest.php
@@ -4,7 +4,7 @@ namespace Fenom;
 use Fenom,
     Fenom\Render;
 
-class RenderTest extends \PHPUnit_Framework_TestCase
+class RenderTest extends TestCase
 {
 
     /**
